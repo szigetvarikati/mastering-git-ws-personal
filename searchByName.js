13fdf30bf8e99e438b414e2826661b93f20b9bfe
szigetvarikati <szigetvarikati@gmail.com>
2023-02-07 11:21:37 +0100

    implement the searchByname function

--- searchByName.js
+++ searchByName.js
@@ -3,7 +3,10 @@ const getBeers = require('./data')
 
 function main(keyword) {
     console.log('keyword:', keyword)
-    // Tip: string.prototype.includes()
+    const beers = getBeers()
+    const searchResult = beers.filter((beer) => beer.name.includes(keyword));
+        searchResult.forEach((beer) => console.log(beer.name))
+
 }
 
 main(process.argv[2])
