24f5f095bf0eeaf6e06ebde60c6cbc82732c43b7
szigetvarikati <szigetvarikati@gmail.com>
2023-02-07 11:41:26 +0100

    implement the filterByType function

--- filterByType.js
+++ filterByType.js
@@ -2,7 +2,11 @@ const getBeers = require('./data')
 
 function main(type) {
     console.log('type:', type)
-
+    const beers = getBeers()
+    const filtered = beers.filter(function (beer) {
+        return beer.type.includes(type)
+    })
+    filtered.forEach(function (beer) {console.log(beer.name) })
 }
 
 main(process.argv[2])
